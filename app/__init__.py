eb79a2f50375edd33bfafc09eec81f1102f910d5
Gaurav Kulkarni <gauravkulkarni96@gmail.com>
2020-10-03 14:12:54 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from config import SECRET_KEY
 app=Flask(__name__, static_url_path='/app/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql6154804:9dUb6DvGFd@sql6.freesqldatabase.com/sql6154804'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<username>:<user>@<db>'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
 app.secret_key = SECRET_KEY
 from models import db
